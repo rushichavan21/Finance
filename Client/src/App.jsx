dabc67e2119bcfc78d27a63b2b611f266ef2366a
rushichavan21 <rushichavan2327@gmail.com>
2025-02-16 17:34:13 +0530

    few routes edits

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,8 @@ import {Routes, Route, useLocation} from 'react-router-dom'
 import InProcess from './pages/inprocess/InProcess'
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup'
-
+import Footer from './components/footer/Footer'
+import RoadmapLanding from './components/roadmap/RoadmapLanding'
 const App = () => {
   const location = useLocation();
   const isAuthPage = ['/login', '/signup'].includes(location.pathname);
@@ -27,11 +28,12 @@ const App = () => {
           <Route path='/news' element={<InProcess/>}/>
           <Route path='/tracker' element={<InProcess/>}/>
           <Route path='/crypto' element={<InProcess/>}/>
-          <Route path='/roadmap' element={<InProcess/>}/>
+          <Route path='/roadmap' element={<RoadmapLanding/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </div>
+      {!isAuthPage && <Footer />}
     </div>
   )
 }
