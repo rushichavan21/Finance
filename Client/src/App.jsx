04dc5944515759d00f15e0dabd25208d40b1c0eb
rushichavan21 <rushichavan2327@gmail.com>
2025-02-16 10:37:45 +0530

    added routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,26 @@
 import Navbar from './components/navbar/Navbar'
 import Options from './components/options/Options'
 import Home from './pages/home/Home'
-
+import {Routes,Route} from 'react-router-dom'
+import InProcess from './pages/inprocess/InProcess'
 const App = () => {
   return (
     <div>
-      <div className="page-container">
       <Navbar/> 
       <Options/>
-      <Home/>
+      <div className="page-container">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/watchlist' element={<InProcess/>}/>
+          <Route path='/portfolio' element={<InProcess/>}/>
+          <Route path='/news' element={<InProcess/>}/>
+          <Route path='/tracker' element={<InProcess/>}/>
+          <Route path='/crypto' element={<InProcess/>}/>
+          <Route path='/roadmap' element={<InProcess/>}/>
+        </Routes>
       </div>
+
     </div>
   )
 }
