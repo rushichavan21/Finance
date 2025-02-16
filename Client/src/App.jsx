2ca63dde96454c198e5fd843ea1765e8ba468337
rushichavan21 <rushichavan2327@gmail.com>
2025-02-16 12:59:56 +0530

    new routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,24 @@
-
 import Navbar from './components/navbar/Navbar'
 import Options from './components/options/Options'
 import Home from './pages/home/Home'
-import {Routes,Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import InProcess from './pages/inprocess/InProcess'
+import Login from './pages/login/login'
+import Signup from './pages/signup/signup'
+
 const App = () => {
+  const location = useLocation();
+  const isAuthPage = ['/login', '/signup'].includes(location.pathname);
+
   return (
     <div>
-      <Navbar/> 
-      <Options/>
-      <div className="page-container">
+      {!isAuthPage && (
+        <>
+          <Navbar/> 
+          <Options/>
+        </>
+      )}
+      <div className={`page-container ${isAuthPage ? 'auth-page' : ''}`}>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/home' element={<Home/>}/>
@@ -19,9 +28,10 @@ const App = () => {
           <Route path='/tracker' element={<InProcess/>}/>
           <Route path='/crypto' element={<InProcess/>}/>
           <Route path='/roadmap' element={<InProcess/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </div>
-
     </div>
   )
 }
