2459702c1e3c0d7f2a538740e1256cdbd8ffa925
rushichavan21 <rushichavan2327@gmail.com>
2025-02-16 10:37:28 +0530

    development page added

---
+++ Client/src/pages/inprocess/InProcess.jsx
@@ -0,0 +1,12 @@
+import "./inprocess.css"
+const InProcess = () => {
+  return (
+    <div className="inprocess-container">
+        <div className="inprocess-div">
+            Currently in development phase
+        </div>
+    </div>
+  )
+}
+
+export default InProcess
