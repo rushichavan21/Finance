a1fd57a7f6dada18fcf0dbe9bb37c8def01dffd9
rushichavan21 <rushichavan2327@gmail.com>
2025-02-19 03:46:56 +0530

    divs alignment

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Commodities from "../../components/commodities/Commodities";
 import CurrencyChart from "../../components/currencyChart/CurrencyChart";
 import IndicesData from "../../components/indices/indicesData";
 import NewsSection from "../../components/newsSection/newsSection";
+import Quote from "../../components/quote/Quote";
 import "./home.css";
 const Home = () => {
   return (
@@ -20,8 +21,13 @@ const Home = () => {
            <CurrencyChart/>
             </div>
           </div>
+          <div className="compartment-home">
           <div className="commodity-section">
             <Commodities/>
+          </div>
+          <div className="quote-section">
+          <Quote/>  
+          </div>
           </div>
           </div>
         </div>
