e64f4b56912e8fd0ad6e5b63535128b082df3376
rushichavan21 <rushichavan2327@gmail.com>
2025-02-18 18:38:08 +0530

    fetched  and mapped data to client

--- Client/src/components/indices/IndicesData.jsx
+++ Client/src/components/indices/IndicesData.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './indices.css';
+import IndexCard from './indexCard/IndexCard';
 
 const IndicesData = () => {
   const [data, setData] = useState([]);
@@ -11,9 +12,16 @@ const IndicesData = () => {
     const fetchIndicesData = async () => {
       try {
         const response = await axios.get('http://localhost:3000/api/nse-indices');
-        setData(response.data);
-        setLoading(false);
         console.log(response.data);
+        if (response.data && Array.isArray(response.data.data)) {
+          setData(response.data.data);
+        } else if (Array.isArray(response.data)) {
+          setData(response.data);
+        } else {
+          setData([]);
+        }
+        
+        setLoading(false);
       } catch (err) {
         setError(err.message || 'An error occurred while fetching data');
         setLoading(false);
@@ -22,19 +30,51 @@ const IndicesData = () => {
     };
 
     fetchIndicesData();
-  }, []);
+  }, []);  
   return (
     <div className="indicesData-wrapper">
-    <div className="indice-left">
-    <div className="indexCard">Nifty</div>
-    <div className="indexCard">Nifty Next 50</div>
+      <div className="indice-left">
+        {data.length > 0 && (
+          <IndexCard
+            name={data[0].name}
+            value={data[0].value}
+            change={data[0].change}
+            percentage={data[0].percentage}
+            trend={data[0].trend}
+          />
+        )}
+        {data.length > 1 && (
+          <IndexCard
+            name={data[1].name}
+            value={data[1].value}
+            change={data[1].change}
+            percentage={data[1].percentage}
+            trend={data[1].trend}
+          />
+        )}
+      </div>
+      <div className="indice-right">
+        {data.length > 2 && (
+          <IndexCard
+            name={data[2].name}
+            value={data[2].value}
+            change={data[2].change}
+            percentage={data[2].percentage}
+            trend={data[2].trend}
+          />
+        )}
+        {data.length > 3 && (
+          <IndexCard
+            name={data[3].name}
+            value={data[3].value}
+            change={data[3].change}
+            percentage={data[3].percentage}
+            trend={data[3].trend}
+          />
+        )}
+      </div>
     </div>
-    <div className="indice-right">
-    <div className="indexCard">BankNifty</div>
-    <div className="indexCard">Nifty Midcap select</div>
-    </div>
-    </div>
-  )
+  );
 };
 
 export default IndicesData;
