c8a6212dd0f9c5c204e84daa225c0bb69426891a
rushichavan21 <rushichavan2327@gmail.com>
2025-02-19 03:10:34 +0530

    server update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const {scrapeNSEData} =require("./controllers");
+const {scrapeNSEData ,scrapeCommodityPrices} =require("./controllers");
 app.use(express.json());
 app.use(
   cors({
@@ -15,6 +15,21 @@ app.get("/api/nse-indices", async (req, res) => {
   try {
     const data = await scrapeNSEData();
     res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ 
+      success: false, 
+      message: 'Failed to fetch NSE data',
+      error: error.message
+    });
+  }
+});
+
+app.get("/api/commodities", async (req, res) => {
+  try {
+    const data = await scrapeCommodityPrices();
+    res.json({ success: true, data });
+    console.log(data);
+    console.log("hii");
   } catch (error) {
     res.status(500).json({ 
       success: false, 
