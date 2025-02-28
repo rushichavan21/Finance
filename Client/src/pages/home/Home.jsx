c1c8035acafb8a905258bdd998b93f77ea484355
rushichavan21 <rushichavan2327@gmail.com>
2025-02-28 16:14:51 +0530

    home responsive -p1

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,44 +1,59 @@
+import Aboutus from "../../components/aboutUs/Aboutus"; 
+import Commodities from "../../components/commodities/Commodities"; 
+import CurrencyChart from "../../components/currencyChart/CurrencyChart"; 
+import IndicesData from "../../components/indices/indicesData"; 
+import NewsSection from "../../components/newsSection/newsSection"; 
+import Quote from "../../components/quote/Quote"; 
+import "./home.css"; 
+import { useEffect, useState } from "react";
 
-import Aboutus from "../../components/aboutUs/Aboutus";
-import Commodities from "../../components/commodities/Commodities";
-import CurrencyChart from "../../components/currencyChart/CurrencyChart";
-import IndicesData from "../../components/indices/indicesData";
-import NewsSection from "../../components/newsSection/newsSection";
-import Quote from "../../components/quote/Quote";
-import "./home.css";
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
     <div className="home-container">
       <div className="home-content-wrapper">
-       <NewsSection/>
+        {!isMobile && <NewsSection />}
         <div className="home-right-section">
           <div className="index-insights">
             <IndicesData/>
           </div>
+          {isMobile && <NewsSection />}
           <div className="currency-commodity-section">
-          <div className="rupee-vs-dollor-div">
-            <div className="currency-title">Dollor vs Rupee</div>
-            <div className="currency-comp">
-           <CurrencyChart/>
+            <div className="rupee-vs-dollor-div">
+              <div className="currency-title">Dollor vs Rupee</div>
+              <div className="currency-comp">
+                <CurrencyChart/>
+              </div>
             </div>
-          </div>
-          <div className="compartment-home">
-            <div className="compartment-hori">
-          <div className="commodity-section">
-            <Commodities/>
-          </div>
-          <div className="home-aboutus">
-            <Aboutus/>
-          </div>
+            <div className="compartment-home">
+              <div className="compartment-hori">
+                <div className="commodity-section">
+                  <Commodities/>
+                </div>
+                <div className="home-aboutus">
+                  <Aboutus/>
+                </div>
+              </div>
+              <div className="quote-section">
+                <Quote/>
+              </div>
             </div>
-          <div className="quote-section">
-          <Quote/>  
-          </div>
-          </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default Home;
